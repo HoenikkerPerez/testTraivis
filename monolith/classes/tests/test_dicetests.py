b7422ff9ea0f3f766167a3d35e3594387235d9f1
HoenikkerPerez <lucaperetti.lp@gmail.com>
2019-10-24 17:58:59 +0200

    Update test_dicetests.py

--- monolith/classes/tests/test_dicetests.py
+++ monolith/classes/tests/test_dicetests.py
@@ -2,9 +2,8 @@ from monolith.classes.DiceSet import Die
 import unittest
  
 class TestDie(unittest.TestCase):
- 
     def test_add_integers_positive(self):
-        die = Die("die0.txt")
+        die = Die("./monolith/classes/tests/die0.txt")
         rnd.seed(574891)
         print(die.faces)
         self.assertEqual(result, 3)
